39b689a0dba69931669ff290895b04276d794607
Zion Falk <zion.falk@elev.ga.ntig.se>
2025-03-20 12:07:37 +0100

    turn order works

--- slutProj/BattleSystem.cs
+++ slutProj/BattleSystem.cs
@@ -11,18 +11,27 @@ public class BattleSystem
         fighters = new List<Fighter>();
         fighters.AddRange(heroes);
         fighters.AddRange(enemies);
+
+        //Skapar AV värde för allt i fighters listan.
         fighters.ForEach(f => f.AV = 10000/f.SPD);
     }
 
     public void InBattle()
-    {
+    {   
+        //Skapar en while loop för när någon i bege listorna är levande
         while(fighters.Any(h => h is Hero && h.HP > 0) && fighters.Any(e => e is Enemy && e.HP > 0))
         {
+            //Anordnar fighters listan från lägst AV value och sen gör det till listan.
+            // Den figther som har lägst AV kommer att bli nämnd till currentFigther
             fighters = fighters.OrderBy(f => f.AV).ToList();
             Fighter currentFighter = fighters.First();
-            fighters.ForEach(f => f.AV -= currentFighter.AV);
+
+
+            // letar inom fighters listan för där f inte är currentfighter och exluderar det från listan. sedan återskapar listan
+            // och för allt inom listan (nu finns inte currentfighter) så gör den minus currentFighter.AV på allt.
+            fighters.Where(f => f != currentFighter).ToList().ForEach(f => f.AV -= currentFighter.AV);
             currentFighter.AV = 0;
-            
+
             if (currentFighter is Hero hero)
                 HeroTurn(hero);
             else if (currentFighter is Enemy enemy)
@@ -39,9 +48,11 @@ public class BattleSystem
         Console.WriteLine($"It is {hero.Name}'s turn!");
         Console.WriteLine("1. Normal Attack  2. Skill Attack  3. Ultimate Attack");
 
-        Enemy target = fighters.OfType<Enemy>.FirstOrDefault(e => e.HP > 0);
+        // Attackerar den förta enemy:n inom listan eftersom jag har inte kommit runt till att skriva kod för att välja. Det kommer senare.
+        Enemy target = fighters.OfType<Enemy>().FirstOrDefault(e => e.HP > 0);
         if (target == null) return;
 
+        
         while (!choiceString.All(char.IsDigit) && choiceInt >= 4 || choiceInt <= 0)
         {
             choiceString = Console.ReadLine();
@@ -51,25 +62,29 @@ public class BattleSystem
         {
             case 1:
                 hero.Attack(target);
-                hero.AV += 10000 / hero.SPD;
+                hero.AV = 10000 / hero.SPD;
                 break;
             case 2:
                 hero.SkiAttack(target);
-                hero.AV += 10000 / hero.SPD;
+                hero.AV = 10000 / hero.SPD;
                 break;
             case 3:
                 hero.UltAttack(target);
-                hero.AV += 10000 / hero.SPD;
+                hero.AV = 10000 / hero.SPD;
                 break;
         }
-        Console.WriteLine($"{target.HP}'s HP: {target.HP}");
+        Console.WriteLine($"{target.Name}'s HP: {target.HP}");
+        Console.ReadLine();
     }
     public void EnemyTurn(Enemy enemy)
     {
-        Hero target = fighters.OfType<Hero>.FirstOrDefault(h => h.HP > 0);
+        Hero target = fighters.OfType<Hero>().FirstOrDefault(h => h.HP > 0);
+        if (target == null) return;
+
         Console.WriteLine($"It is {enemy.Name}'s turn!");
         Console.ReadLine();
         enemy.Attack(target);
+        enemy.AV = 10000/ enemy.SPD;
         Console.WriteLine($"{target.Name}'s HP: {target.HP}");
     }
 }
--- slutProj/Program.cs
+++ slutProj/Program.cs
@@ -1,2 +1,26 @@
-﻿
+﻿using System;
+using System.Media;
 
+
+
+
+
+
+
+
+Hero player = new Hero { Name = "Player", HP = 100, ATK = 20, SPD = 100, CR = 20, CD = 150, NorMult = 100, SkiMult = 200, UltMult = 400 };
+Hero player2 = new Hero { Name = "Player2", HP = 100, ATK = 20, SPD = 89, CR = 20, CD = 150, NorMult = 100, SkiMult = 200, UltMult = 400 };
+List<Hero> heroes = new();
+heroes.Add(player);
+heroes.Add(player2);
+List<Enemy> enemies = new List<Enemy> { new Enemy { Name = "Goblin", HP = 5000000, ATK = 10, SPD = 90 }, new Enemy { Name = "Goblin2", HP = 5000000, ATK = 10, SPD = 90 } };
+BattleSystem combat = new BattleSystem(heroes, enemies);
+combat.InBattle();
+
+
+
+
+static void PlayMusic(int song)
+{
+    
+}
