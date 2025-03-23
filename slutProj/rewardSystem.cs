c0fb8bfe7863f7139ae26ba1389fd8fdc952d2db
Zion Falk <zion.falk@elev.ga.ntig.se>
2025-03-23 16:29:30 +0100

    Added item descriptions.

--- slutProj/rewardSystem.cs
+++ slutProj/rewardSystem.cs
@@ -14,21 +14,21 @@ public class rewardSystem
     //Speed is additive, CR is additive, CD is additive,
     //ATK is multiplicitive, HP is multiplicitave.
     new Buff("Air Jordans",15,0,0,500,500,
-    "jordn"),
+    $"These boots gives your team +15 SPD as well as +5% HP & ATK."),
     new Buff("Lens-Maker's Glasses",0,7,10,100,100,
-    "gls"),
+    "These glasses gives your team +7 CR as well as +10 CD."),
     new Buff("Compact Nuke",0,0,75,0,0,
-    "nuk"),
+    "A compact nuke that increases CD by +75%"),
     new Buff("Portable Tank",0,0,20,2000,3000,
-    "tank"),
+    "If they're so strong why not carry them?\nIncreases CD by +20%, ATK by +20% and HP by +30%"),
     new Buff("Just ATK",0,0,0,4000,0,
-    "atk"),
+    "What? It's just ATK.\n Increases ATK by 40%"),
     new Buff("It's my turn.",40,0,0,100,100,
-    "tun"),
+    "Ha ha ha, one!\n Increases SPD by +40 for all teammates"),
     new Buff("Filler",0,5,10,200,200,
-    "fil"),
+    "It's just here.. I guess?\n Increases CR by 5%, CD by 10%, ATK by 2% and HP by 2%."),
     new Buff("Dev Spec",100,50,300,20000,100,
-    "spc")
+    "You aren't supposed to have this...\n +100 SPD, +50 CR, +300 CD, +200% ATK")
    };
 
    
@@ -57,8 +57,9 @@ public class rewardSystem
                 Console.WriteLine("You are being rewarded for your efforts.\nThree random items will be generated.\nYou will be able to pick one of these for yourself.");
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("\n TIP: You are able to select once to see what buffs they give and then back out if you dont wan't it.");
+                Console.ResetColor();
                 Console.ReadLine();
-                Console.ResetColor();   
+                
             
                 for (int i = 0; i < buffs.Count; i++)
                 {
@@ -87,9 +88,12 @@ public class rewardSystem
 
 
     }
-    public void RewardTrade()
+    public string YellowNumber(string input)
     {
-
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write($"{input}");
+        Console.ResetColor();
+        return input;
     }
 
     //Skapar en ny lista med en method så vi kan använda den i RewardBuff()
