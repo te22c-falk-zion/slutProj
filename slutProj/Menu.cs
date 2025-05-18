c641a0cde66fde4f932761ea26c9ac3ce5999208
Zion Falk <zion.falk@elev.ga.ntig.se>
2025-05-18 19:12:18 +0200

    Added dictionary in menu class

--- slutProj/Menu.cs
+++ slutProj/Menu.cs
@@ -1,5 +1,58 @@
 public class Menu {
 
+    private Dictionary<int, string[]> helpStrings = new Dictionary<int, string[]>
+    {
+        { 1, new string[] {
+            "-- Welcome to NOT star rail --",
+            "In this game you will fight against enemies until both your heroes are dead.",
+            "Clearing a wave will reward you with a selection of buffs to choose from.",
+            "-- Normal Attack --",
+            "Normal attacks deal the least amount of damage in your heroes' kit.",
+            "After using a normal attack you will gain a skill point.",
+            "-- Skill Attack --",
+            "Skill points are used to cast Skill Attacks!",
+            "Prioritise using skill attacks with heroes that have higher damage output to clear waves faster!",
+            "-- Ultimate Attack --",
+            "Using attacks and getting hit will generate a hero's Ultimate Energy.",
+            "When Ultimate Energy is full a hero can use their Ultimate Attack to deal HUGE damage!",
+            "-- Buffs --",
+            "Buffs are items picked up that increase the strength of your heroes.",
+            "Buffs can be obtained randomly in occurances or when winning battles.",
+            "-- Occurrences --",
+            "Occurrences randomly occur at the start or end of your fights.",
+            "They can either heal, buff, or nerf you depending on what you get."
+        }},
+        { 2, new string[] {
+            "-- Welcome to NOT star rail --",
+            "In this game you will fight against enemies until both your heroes are dead.",
+            "Clearing a wave will reward you with a selection of buffs to choose from."
+        }},
+        { 3, new string[] {
+            "-- Normal Attack --",
+            "Normal attacks deal the least amount of damage in your heroes' kit.",
+            "After using a normal attack you will gain a skill point."
+        }},
+        { 4, new string[] {
+            "-- Skill Attack --",
+            "Skill points are used to cast Skill Attacks!",
+            "Prioritise using skill attacks with heroes that have higher damage output to clear waves faster!"
+        }},
+        { 5, new string[] {
+            "-- Ultimate Attack --",
+            "Using attacks and getting hit will generate a hero's Ultimate Energy.",
+            "When Ultimate Energy is full a hero can use their Ultimate Attack to deal HUGE damage!"
+        }},
+        { 6, new string[] {
+            "-- Buffs --",
+            "Buffs are items picked up that increase the strength of your heroes.",
+            "Buffs can be obtained randomly in occurances or when winning battles."
+        }},
+        { 7, new string[] {
+            "-- Occurrences --",
+            "Occurrences randomly occur at the start or end of your fights.",
+            "They can either heal, buff, or nerf you depending on what you get."
+        }}
+    };
 public int MainMenuChoose(float stage)
 {
     string choiceString = "a";
@@ -51,48 +104,18 @@ public void Help()
 
 public void Intro(int var)
 {   
-        
-    if (var == 1 || var == 2)
-    {
-        Console.WriteLine("-- Welcome to NOT star rail --");
-        Console.WriteLine("In this game you will fight against enemies until both your hereos are dead.");
-        Console.WriteLine("Clearing a wave will reward you with a selection of buffs to choose from.");
-        Console.ReadLine(); 
-    }
-    if (var == 1 || var == 3)
-    {
-        Console.WriteLine("-- Normal Attack --");
-        Console.WriteLine("Normal attacks deal the least amount of damage in your heroes kit.");
-        Console.WriteLine("After using a normal attack you will gain a skill point");
-        Console.ReadLine(); 
-    }
-    if (var == 1 || var == 4)
-    {
-        Console.WriteLine("-- Skil attack --");
-        Console.WriteLine("Skill points are used to cast Skill Attacks!");
-        Console.WriteLine("Prioritise using skill atatcks with heroes that have higher damage output to clear waves faster!");
-        Console.ReadLine();
-    }
-    if (var == 1 || var == 5)
-    {
-        Console.WriteLine("-- Ultimate Attack --");
-        Console.WriteLine("Using attacks and getting hit will generate a heroes Ultimate Enegy");
-        Console.WriteLine("When Ultimate Energy is full a hero can use their Ultimate Attack to deal HUGE damage to enemies!");
-        Console.ReadLine();
-    }
-    if (var == 1 || var == 6)
-    {
-        Console.WriteLine("-- Buffs --");
-        Console.WriteLine("Buffs are items picked up that increase the strenght of your heroes.");
-        Console.WriteLine("Buffs can be obtained randomly in occurances or when winning battles.");
-        Console.ReadLine();
-    }
-    if (var == 1 || var == 7)
-    {
-        Console.WriteLine("-- Occurances --");
-        Console.WriteLine("Occurances randomly occur on either the start or end of your figths.");
-        Console.WriteLine("Occurances can either heal, buff or nerf you depending on what you get.");
-        Console.ReadLine();
-    }
+    if (helpStrings.ContainsKey(var))
+        {
+            foreach (var text in helpStrings[var])
+            {
+                Console.WriteLine(text);
+            }
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Invalid number.\nQuitting to menu...");
+            Console.ReadLine();
+        }
 }
 }
